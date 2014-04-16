8c19cf5bfd05144838637f9d671780e0cb3decd5
Aarskin <mwiemer@outlook.com>
2014-04-16 16:48:48 -0500

    testbenches not in tarball

---
+++ testbenches/CPU_tb.v
@@ -0,0 +1,19 @@
+module CPU_tb();
+
+reg clk, rst_n;
+wire hlt;
+wire [15:0] addr;
+
+CPU iDUT(.clk(clk), .rst_n(rst_n), .hlt(hlt), .pc(addr));
+
+initial begin
+	clk = 1'b0;
+
+	rst_n = 1'b0;
+	#2;
+	rst_n = 1'b1;
+end
+
+always #1 clk = ~clk; 
+ 
+endmodule
