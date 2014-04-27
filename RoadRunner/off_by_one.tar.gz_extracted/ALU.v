83aee01f2011bd9985795f23fc33b142723f3138
Aarskin <mwiemer@outlook.com>
2014-04-27 13:42:28 -0500

    dense hazard test

--- RoadRunner/off_by_one.tar.gz_extracted/ALU.v
+++ RoadRunner/off_by_one.tar.gz_extracted/ALU.v
@@ -3,9 +3,9 @@ module ALU(src0, src1, ctrl, shamt, result, ov, zr, ne);
   input [15:0] src0, src1;
   input [2:0] ctrl;
   input [3:0] shamt;
-	
+  
   output [15:0] result;
-	output ov, zr, ne;
+  output ov, zr, ne;
 
   wire [15:0] unsat, op1;
 
@@ -25,16 +25,20 @@ module ALU(src0, src1, ctrl, shamt, result, ov, zr, ne);
                  (ctrl==nory)? ~(src0|src1):
                  (ctrl==sll) ? src0<<shamt:
                  (ctrl==srl) ? src0>>shamt:
-		             (ctrl==sra) ? {$signed(src0) >>> shamt}:
+                 (ctrl==sra) ? {$signed(src0) >>> shamt}:
                  17'h00000; // It will never reach here logically
 
- 	// When checking msbs for overflow, we need the actual bits operated on
-	assign op1 = ctrl==sub ? ~src1 + 1'b1 : src1;          
+  // When checking msbs for overflow, we need the actual bits operated on
+  // Complementing 0x8000 => 0x8000, so set it to 0x0000 to make it positive instead
+  assign op1 = ctrl==sub ? ((src1==16'h8000) ? 16'h0000 : ~src1 + 1'b1) : src1;          
 
   // Positive operands; Negative result
-	assign negativeOverflow =(src0[15] && op1[15] && !unsat[15]);
+  // There is a corner case when subtracting with 0x8000, since the positive complement doesn't exist
+  assign subCornerCase = !(ctrl==sub && src1 == 16'h8000 && src0[15]); 
+  assign negativeOverflow = (src0[15] && op1[15] && !unsat[15] && subCornerCase);
+  
   // Negative operands; Positive result
-	assign positiveOverflow = (!src0[15] && !op1[15] && unsat[15]);
+  assign positiveOverflow = (!src0[15] && !op1[15] && unsat[15]);
 
   // Set Result
   assign result = (positiveOverflow && (ctrl==add || ctrl==sub)) ? 16'h7fff :
